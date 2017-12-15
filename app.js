6e1f763422ec70418c98ce89f7a0f15717695dc1
yayo <yangyong@navercorp.com>
2017-12-15 13:28:08 +0800

    get openid

--- app.js
+++ app.js
@@ -21,6 +21,23 @@ App({
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        let self = this;
+        let appId = 'wxd6a1ad51140d5134';
+        let appSecret = '9e1afbd7ae62f4891f8cacafa42d9582';
+
+        wx.request({
+          url: 'https://api.weixin.qq.com/sns/jscode2session',
+          method: 'GET',
+          data: {
+            appId: appId,
+            secret: appSecret,
+            js_code: res.code,
+            grant_type: 'authorization_code'
+          },
+          success: function (data) {
+            self.globalData.openId = data.data.openid;
+          }
+        })
       }
     })
     // 获取用户信息
@@ -46,6 +63,7 @@ App({
   },
   globalData: {
     userInfo: null,
+    openId: '',
     host: 'http://localhost:8888',
   },
   request: {
